869ffea70cb16a25f78a7d5ea43bfee58047fc06
Arto Bendiken <arto@bendiken.net>
2014-10-11 22:53:44 +0200

    Ensured that Sphinx ignores the repository README file,

--- conf.py
+++ conf.py
@@ -71,7 +71,7 @@ release = '2013-09-17'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['.build']
+exclude_patterns = ['.build', 'README.rst']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
